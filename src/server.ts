7ef75392cff1e00b1330d1a666152e166a654604
Moises0011 <moisesnascfernandes@gmail.com>
2023-05-29 21:24:11 -0400

    settings

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import { router } from "./routes";
 import { CreateAlunoController } from "./controllers/alunoCreateController";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 app.use(router);
 
 const creatAluno = new CreateAlunoController();
+
 router.post("/createAluno", creatAluno.handle);
 
 app.listen(4003, () => console.log("Servidor está em execução on PORT 4003"));
